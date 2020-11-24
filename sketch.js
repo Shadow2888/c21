cd5e2495ca98c32088a3765c3f93e651cd706ba7
Shadow2888 <72982446+Shadow2888@users.noreply.github.com>
2020-11-24 10:23:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,37 @@
-var b,a;
-var s ,w ,t ;
+var c,w;
+var sp,wg,th;
 function setup() {
   createCanvas(1600,400);
-  b = createSprite(50, 200, 40, 20);
-  a = createSprite(1500, 200, t, 200);
-
-
-}
-
-function draw() {
-  background("black");  
-  s = random(223,321);
- w = random(30,52);
- t = random(22,83);
-  b.velocityX = s ;
+  sp=random(55,90);
+  wg=random(30,52);
+  th=random(22,83);
+  w = createSprite(1500, 200,th, 200);
+ c = createSprite(1400,200,30,20);
+ c.velocityX = sp;
   
  
+
+ 
+
+}
+ 
+//c.x = mouseX;
+//c.y = mouseY;
+
+
+
+function draw() {
+  background(0);  
+  if(w.x - c.x < (c.width + w.width)/2){
+    c.velocityX = 0;
+  var dft =0.5 * sp * wg * sp/25509;
+if(dft > 180){
+  w.shapeColor=color(255,0,0);
+}
   
-  
-  drawSprites();
+if(dft < 180){
+  w.shapeColor=color(0,255,0);
+}
+}
+drawSprites();
 }
